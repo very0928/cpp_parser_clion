584bf4c6c0f29b0df15959ba4988b7b4183ca514
GitHub Action <action@github.com>
2024-06-30 03:24:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/very0928/cppparserclion/MyBundle.kt
+++ src/main/kotlin/com/github/very0928/cppparserclion/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.very0928.cppparserclion
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/very0928/cppparserclion/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/very0928/cppparserclion/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.very0928.cppparserclion.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
